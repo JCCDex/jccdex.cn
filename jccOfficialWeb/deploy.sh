d9b3db924aec6ed4445443d4b5a4e517b7431e11
wjisi <7042783@qq.com>
2023-10-20 12:26:36 +0800

    fix deploy.sh

--- jccOfficialWeb/deploy.sh
+++ jccOfficialWeb/deploy.sh
@@ -7,4 +7,5 @@ npm run build
 # 拷贝到服务器
 rsync -avz -e 'ssh -p 6422' dist/ front@58.243.201.56:~/frontcode/jcc_officialWeb/dist
 
-# 服务器地址 https://jccdex.cn/
+# 测试环境： https://whwww.jccdex.cn:8443
+# 正式环境： https://jccdex.cn/
