b730accb03f98d2541975db8f80dcf2cc822216c
wjisi <7042783@qq.com>
2023-12-20 13:39:32 +0800

    fix deploy

--- jccOfficialWeb/deploy.sh
+++ jccOfficialWeb/deploy.sh
@@ -11,4 +11,5 @@ rsync -avz -e 'ssh -p 6422' dist/ front@58.243.201.56:~/frontcode/jcc_officialWe
 # 正式环境： https://jccdex.cn/
 
 # 更新正式环境 会将jcc_documents正式环境一起同步，请确定jcc_documents项目已经上传了最新代码并测试没问题
-# param='web=www-jccdex-cn';job="front-code-sync" ;token="115574c30a3084ab6aac18e1b005842a68";curl -X POST "https://whjenkins.jccdex.cn:8443/job/${job}/buildWithParameters?${param}" --user "afd:${token}"
+# 查看更新记录： https://jccdex.cn/health.json
+# 更新命令： param='web=www-jccdex-cn';job="front-code-sync" ;token="115574c30a3084ab6aac18e1b005842a68";curl -X POST "https://whjenkins.jccdex.cn:8443/job/${job}/buildWithParameters?${param}" --user "afd:${token}"
